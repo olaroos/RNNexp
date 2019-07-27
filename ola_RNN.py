added accuracy calculation, it's not correct at the moment because I cut down the input when tweets have ended so accuracy will be off because of that. Fix this tomorrow

ola_RNN.py
@@ -6,6 +6,7 @@ import re
 import numpy as np
 import torch, torchvision
 import torch.nn as nn
+from functools import partial
 
 def cuda(input):
     if torch.cuda.is_available(): return input.cuda()
@@ -196,15 +197,18 @@ def get_valid_rnn(learn,itters=30):
     print(f"""getting validation""")    
     learn.model.eval()
     tot_loss = 0 
+    tot_accu = 0 
     with torch.no_grad():
         hidden = learn.model.initHidden(learn.data.valid_dl.bs)
         for xb,yb in iter(learn.data.valid_dl): 
-            output, hidden, loss = learn.model.batch_forward(xb,yb,hidden,learn.loss_fn)
-            if loss != 0: tot_loss += loss.item()/xb.shape[0]
+            output, hidden, loss, accu = learn.model.batch_forward(xb,yb,hidden,learn.loss_fn)
+            if loss != 0: 
+                tot_loss += loss.item()/xb.shape[0]
+                tot_accu += accu/xb.shape[0]
             if learn.data.valid_dl.get_itter() == itters: 
-                return tot_loss/learn.data.valid_dl.get_itter()
+                return tot_loss/learn.data.valid_dl.get_itter(), tot_accu/learn.data.valid_dl.get_itter()
         
-    return tot_loss/learn.data.valid_dl.nb_itters()
+    return tot_loss/learn.data.valid_dl.nb_itters(), tot_accu/learn.data.valid_dl.get_itter()
 
 def generate_seq(model,Data,sql,symbol='^'):
     model.eval()
@@ -259,7 +263,7 @@ class Learner():
         finally:                                           self('after_batch')
 
 def one_rnn_batch(xb,yb,cb):
-    pred, cb.learn.hidden, loss = cb.learn.model.batch_forward(xb,yb,cb.learn.hidden,cb.learn.loss_fn)
+    pred, cb.learn.hidden, loss, accu = cb.learn.model.batch_forward(xb,yb,cb.learn.hidden,cb.learn.loss_fn)
     if not cb.after_loss(loss): return    
     loss.backward()
     if not cb.after_backward(): return 

ola_models.py
@@ -2,6 +2,11 @@ import torch, torchvision
 import torchvision.models as models
 import torchvision.transforms as transforms
 import torch.nn as nn
+def get_accu(output,y):
+    _, idxs = output.max(1)
+    n_correct = ((idxs-y)==0).sum().item()
+    accu = n_correct/output.shape[1]
+    return accu
 
 def cuda(input):
     if torch.cuda.is_available(): return input.cuda()
@@ -23,9 +28,10 @@ class SGRU(nn.Module):
         self.GRUs = []
         for i in range(n_stacks):
             self.GRUs.append(cuda(GRU(in_sz,hd_sz)))
-                
-    def batch_forward(self,xb,yb,hidden_in,loss_fn):
+            
+    def batch_forward(self,xb,yb,hidden_in,loss_fn,calc_acc=False):
         if xb[0,0,1].item() == 1 or hidden_in is None: hidden_in = self.initHidden(xb.shape[0])                   
+        accu = 0
         loss = 0
         for char in range(0,xb.shape[1]):
             x,y           = xb[:,char],yb[:,char]
@@ -35,10 +41,11 @@ class SGRU(nn.Module):
             for stack in range(self.n_stacks-1):
                 gru = self.GRUs[stack]
                 x, hidden_out[stack] = gru.forward(x,hidden_in[stack],True)
-            output, hidden_out[stack+1] = gru.forward(x,hidden_in[char],False)
+            output, hidden_out[stack+1] = gru.forward(x,hidden_in[stack+1],False)
             hidden_in = hidden_out.clone()
+            accu += get_accu(output,y)    
             loss += loss_fn(output,y)
-        return output, hidden_out.detach(), loss/(char+1)
+        return output, hidden_out.detach(), loss/(char+1), accu
         
     def initHidden(self, bs): return cuda(torch.zeros(self.n_stacks,bs,self.hd_sz))
     
@@ -84,8 +91,6 @@ class GRU(nn.Module):
         self.train()
         if xb[0,0,1].item() == 1: hidden = self.initHidden(xb.shape[0])                   
         loss = 0 
-        print(xb.shape)
-        print(xb.shape[1])
         for char in range(xb.shape[1]):
             x,y           = xb[:,char],yb[:,char]
             x,y,hidden    = unpad(x,y,hidden)